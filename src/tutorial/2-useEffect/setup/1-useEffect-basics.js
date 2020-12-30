3ec87a49827dc3513556200a73b84f8e4cac68ba
Vijay Kumar Rajput <vijay.kumar1@sony.com>
2020-12-30 01:21:49 -0800

    UseEfeect1.0

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -3,12 +3,29 @@ import React, { useState, useEffect } from 'react'
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
+  const [value, setValue] = useState(0)
+  const incrementMe = (value) => {
+    setValue(value + 1)
+  }
   console.log('Rendering start!!')
   useEffect(() => {
-    console.log('Use Effeect called once after rendering')
+    console.log('Use Effect called once after rendering')
+    if (value > 0) {
+      document.title = `New Value: ${value}`
+    }
   })
   console.log('Rendering done!!!')
-  return <h1>Use Effect Basis</h1>
+  return (
+    <>
+      <h1>{value}</h1>
+      <button className='btn' onClick={() => incrementMe(value)}>
+        Increment
+      </button>
+      <button className='btn' onClick={() => incrementMe(value - 1 - 1)}>
+        Decrement
+      </button>
+    </>
+  )
 }
 
 export default UseEffectBasics
