39244c272e9c9f5ccb8f085e763c95de3871501e
Vijay Kumar Rajput <vijay.kumar1@sony.com>
2020-12-30 00:57:26 -0800

    UseEfeect

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
-};
+  console.log('Rendering start!!')
+  useEffect(() => {
+    console.log('Use Effeect called once after rendering')
+  })
+  console.log('Rendering done!!!')
+  return <h1>Use Effect Basis</h1>
+}
 
-export default UseEffectBasics;
+export default UseEffectBasics
